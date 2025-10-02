2bf530b49786305a90c552ab8bb997887ce69606
25LP8016 25LP8016 <25LP8016@G305-CC0243.local>
2025-10-02 11:21:41 +0800

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,3 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-// import "./App.css";
-
-// function App() {
-//   const [input, setInput] = useState("");
-//   const [tasks, setTasks] = useState([]);
-//   const [filter, setFilter] = useState("all");
-
-//   const addTask = () => {
-//     const trimmed = input.trim();
-//     if (trimmed === "") return;
-
-//     const newTask = {
-//       id: Date.now(),
-//       text: trimmed,
-//       completed: false,
-//     };
-//     setTasks([...tasks, newTask]);
-//     setInput("");
-//   };
-
-//   const toggleComplete = (id) => {
-//     setTasks(
-//       tasks.map((task) =>
-//         task.id === id ? { ...task, completed: !task.completed } : task
-//       )
-//     );
-//   };
-
-//   const filteredTasks = tasks.filter((task) => {
-//     if (filter === "active") return !task.completed;
-//     if (filter === "completed") return task.completed;
-//     return true;
-//   });
-
-//   return (
-//     <>
-//       <div className="container">
-//         <div className="smallbox">
-//           <h1 className="title">To-Do list </h1>
-//           <div className="inputdiv">
-//             <input
-//               className="inputbox"
-//               type="text"
-//               placeholder="Add a new task..."
-//               value={input}
-//               onChange={(e) => setInput(e.target.value)}
-//               onKeyPress={(e) => e.key === "Enter" && addTask()}
-//             />
-//             <button className="btn" onClick={addTask}>
-//               Add
-//             </button>
-//             <div className="btndiv">
-//               <div>
-//                 <button className="all" onClick={() => setFilter("all")}>
-//                   All
-//                 </button>
-//               </div>
-//               <div>
-//                 <button className="active" onClick={() => setFilter("active")}>
-//                   Active
-//                 </button>
-//               </div>
-//               <div>
-//                 <button className="completed">Completed</button>
-//               </div>
-//             </div>
-//             <p className="text">No tasks yet. Add one above!</p>
-//             <p className="footertxt">
-//               Powered by{" "}
-//               <a className="link" href="https://pinecone.mn/">
-//                 Pinecone academy
-//               </a>
-//             </p>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
 // export default App;
 
 import { useState } from "react";
@@ -145,14 +62,24 @@ function App() {
               Add
             </button>
             <div className="btndiv">
-              <button className="all" onClick={() => setFilter("all")}>
+              <button
+                className={`all btn2 ${filter === "all" ? "activebtn" : ""}`}
+                onClick={() => setFilter("all")}
+              >
                 All
               </button>
-              <button className="active" onClick={() => setFilter("active")}>
+              <button
+                className={`active btn2 ${
+                  filter === "active" ? "activebtn" : ""
+                }`}
+                onClick={() => setFilter("active")}
+              >
                 Active
               </button>
               <button
-                className="completed"
+                className={`completed btn2 ${
+                  filter === "completed" ? "activebtn" : ""
+                }`}
                 onClick={() => setFilter("completed")}
               >
                 Completed
